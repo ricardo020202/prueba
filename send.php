42db92966ad3dcb7b2d1fce4b4ba8738bb33ba21
Ricardo Rosales <ricoroca2@hotmail.com>
2020-07-20 19:58:31 -0500

    prueba 2.0

--- send.php
+++ send.php
@@ -1,7 +1,6 @@
 <?php
 $nombre = $_POST['nombre'];
 $mail = $_POST['email'];
-$empresa = $_POST['mensaje'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -10,7 +9,6 @@ $header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
 $para = 'ricoroca2@hotmail.com';
