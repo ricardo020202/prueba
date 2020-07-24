85fd76cff24f7f81c720b200cee608e95a688681
Ricardo Rosales <ricoroca2@hotmail.com>
2020-07-24 10:31:11 -0500

    3.0

---
+++ send.php
@@ -0,0 +1,20 @@
+<?php
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'ricoroca2@hotmail.com';
+$asunto = 'registro';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+?>
